e5b5145393aed7bb3ceae4796ea8a170addcea70
John Weaver <John@example.com>
2020-06-09 11:39:01 -0400

    Rename min_absolute to min_gb, use parameter names

--- all_checks.py
+++ all_checks.py
@@ -1,2 +1,16 @@
-#all checks.py from blue kale
-def check_disk_full......
+#!/usr/bin/env python3
+import os
+import sys
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+    
+def main():
+	if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    print("Everthing ok.")
+    sys.exit(0)
+	
+main()
